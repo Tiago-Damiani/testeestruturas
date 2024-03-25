42e6182a1574882bfd32d515a40eb43ea5ec4c97
Tiago <tiagocorrea018@gmail.com>
2024-03-25 11:30:56 -0300

    25/03/2024

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <libprg/libprg.h>
-//
+
 int main(void) {
     int a = 1, b = 2;
 
     result_t r = compute(a, b, SUM);
     printf("%d + %d = %8.2f", a, b, r.value);
+
+    criarlista(5);
 
     return 0;
 }
