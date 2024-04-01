2c11a4b61a9aba0cec38de72ff92b3028c3450c4
Tiago <tiagocorrea018@gmail.com>
2024-04-01 11:32:58 -0300

    CriarLista

--- main.c
+++ main.c
@@ -2,11 +2,6 @@
 #include <libprg/libprg.h>
 
 int main(void) {
-    int a = 1, b = 2;
-
-    result_t r = compute(a, b, SUM);
-    printf("%d + %d = %8.2f", a, b, r.value);
-
     criarlista();
 
     return 0;
